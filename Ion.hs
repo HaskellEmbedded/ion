11cbd4861b3ecd827ff96e9db8939462dc4a5814
Chris Hodapp <Hodapp87@gmail.com>
2015-06-20 22:07:03 -0400

    Made some progress with semantics and State stuff.

--- Ion.hs
+++ Ion.hs
@@ -1,68 +1,143 @@
+{- |
+Module: Ion
+Description: Top-level Ion module
+Copyright: (c) 2015 Chris Hodapp
+
+Ion is a Haskell EDSL that is inspired by another EDSL,
+<https://hackage.haskell.org/package/atom Atom>.  Ion aims to be a
+re-implementation of Atom which, rather than generating C code directly (as
+Atom does), interfaces with another very powerful, more general EDSL,
+<http://ivorylang.org/ Ivory>.
+
+-}
+
 module Ion where
 
 import           Control.Monad
 import           Control.Monad.State.Lazy
 
--- | The scheduling state at some point
-data IonState = IonState { ionPeriod :: Int
-                         , ionPhase :: Phase
-                         , ionPath :: [String]
-                         , ionSub :: [Ion ()]
-                         }
+-- | The monad for expressing an Ion specification.
+type Ion a = State IonNode a
 
-instance Show IonState where
-  show st =
-    "IonState {" ++
-    "ionPeriod = " ++ (show $ ionPeriod st) ++
-    ", ionPhase = " ++ (show $ ionPhase st) ++
-    ", ionPath = " ++ (show $ ionPath st) ++
-    ", ionSub = " ++
-    (show $ map (\s -> snd $ runState s defaultIonState) $ ionSub st) ++ "}"
+-- | A node representing some context in the schedule (including its path and
+-- sub-nodes), and the actions this node includes.
+data IonNode = IonNode
+               { ionName :: String -- ^ Name of this node
+               , ionPath :: [String] -- ^ The path to this node (as a list of
+                            -- names, top-level first)
+               , ionSub :: [IonNode] -- ^ Sub-nodes we've accumulated
+               , ionPeriod :: Int -- ^ The current period of the base rate
+               , ionPhase :: Phase -- ^ The current phase within that period
+               , ionAction :: [String] -- ^ Actions to run (this is a dummy
+                              -- type for debugging now)
+               } deriving (Show)
 
-defaultIonState = IonState { ionPeriod = 1
-                           , ionPhase = Phase Absolute Min 1
-                           , ionPath = []
-                           , ionSub = []
-                           }
+-- Speculative ideas:
+-- - I can do a relative phase; what about a relative period? That is, a period
+-- which is relative not to the base rate, but to the last rate that was
+-- inherited?
 
 data Phase = Phase PhaseContext PhaseType Int deriving (Show)
 data PhaseContext = Absolute | Relative deriving (Show)
 data PhaseType = Min | Exact deriving (Show)
 
-type Ion a = State IonState a
+defaultNode = IonNode { ionPeriod = 1
+                      , ionName = "root"
+                      , ionPhase = Phase Absolute Min 1
+                      , ionPath = [ionName defaultNode]
+                      , ionSub = []
+                      , ionAction = []
+                      }
 
-ion :: String -> Ion () -> Ion ()
-ion name ion' = do s <- get
-                   put $ s { ionPath = name : (ionPath s)
-                           , ionSub = ion' : (ionSub s)
-                           }
+-- | Produce a somewhat more human-readable representation of an 'IonNode'.
+prettyPrint :: IonNode -> String
+prettyPrint st =
+  let sub s = join $ map pretty $ ionSub s
+      pretty s = [ "IonNode {"
+                 , " ionName = " ++ (show $ ionName s)
+                 , " ionPath = " ++ (show $ ionPath s)
+                 , " ionPeriod = " ++ (show $ ionPeriod s)
+                 , " ionPhase = " ++ (show $ ionPhase s)
+                 , " ionAction = " ++ (show $ ionAction s)
+                 ] ++
+                 (if null $ ionSub s
+                  then []
+                  else " ionSub =" : (map ("    " ++) $ sub s)) ++
+                 ["}"]
+  in unlines $ pretty st
 
-phase :: Int -> Ion a -> Ion a
-phase i ion' = do
+-- | Create a new named sub-node.
+ion :: String -- ^ Name
+       -> Ion a -- ^ Sub-node
+       -> Ion a
+ion name ion0 = do
   s <- get
-  r <- ion'
-  put $ s { ionPhase = Phase Absolute Min i }
+  -- Run 'ion0' starting from our current state, but with 'ionSub' and
+  -- 'ionAction' cleared (those apply only for the immediate state), a new
+  -- 'ionName', and incremented 'ionPath':
+  let (r, s') = runState ion0 $ s { ionName = name
+                                  , ionPath = ionPath s ++ [name]
+                                  , ionSub = []
+                                  , ionAction = []
+                                  }
+  -- Update our sub-ions with whatever just ran:
+  put $ s { ionSub = ionSub s ++ [s'] }
   return r
 
-period :: Int -> Ion a -> Ion a
-period i ion' = do
+-- | Specify a phase for a sub-node. (The sub-node may readily override this
+-- phase.)
+phase :: Int -- ^ Phase
+         -> Ion a -- ^ Sub-node
+         -> Ion a
+phase i ion0 = do
   s <- get
-  r <- ion'
-  put $ s { ionPeriod = i }
+  let (r, s') = runState ion0 $ s { ionAction = []
+                                  , ionPhase = Phase Absolute Min i
+                                  }
+  put s'
   return r
+-- FIXME: This needs to comprehend the different phase types.
+-- FIXME: This very likely can be written very easily with mapState too.
 
+-- | Specify a period for a sub-node. (The sub-node may readily override this
+-- period.)
+period :: Int -- ^ Period
+          -> Ion a -- ^ Sub-node
+          -> Ion a
+period i ion0 = do
+  s <- get
+  let (r, s') = runState ion0 $ s { ionAction = []
+                                  , ionPeriod = i
+                                  }
+  put s'
+  return r
+-- FIXME: This very likely can be written very easily with mapState, e.g.
+-- mapState $ \(a,s) -> (a, s { ionAction = [], ionPeriod = i })
+-- but the above has some bugs.
+
+-- | Dummy function for specifying an action, which right now is just a string
+-- for the sake of debugging.
+doThing :: String -> Ion ()
+doThing str = do
+  s <- get
+  put $ s { ionAction = ionAction s ++ [str] }
+
+-- | Dummy spec for the sake of testing
 test :: Ion ()
 test = ion "Foo" $ do
 
-  phase 15 $ ion "Bar" $ do
-    return ()
+  doThing "outside"
+  
+  ion "Bar" $ do
+    doThing "foo"
+    doThing "bar"
+    ion "Another" $ do
+      doThing "other_bar"
 
-  period 50 $ ion "Baz" $ do
-    period 100 $ ion "Quux" $ do
-      return ()
-    return ()
+  ion "Baz" $ do
+    doThing "baz"
 
-  return ()
+  period 10 $ phase 5 $ ion "Quux" $ do
+    doThing "quux"
 
-(_, s) = runState test defaultIonState
-
+s = execState test defaultNode
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ion
-An Ivory library inspired by Atom
+# Ion
+
+Ion is heavily experimental.  Don't use it for anything right now.
+
+Ion is a Haskell EDSL that is inspired by another EDSL,
+[https://hackage.haskell.org/package/atom](Atom).  Ion aims to be a
+re-implementation of Atom which, rather than generating C code directly (as
+Atom does), interfaces with another very powerful, more general EDSL,
+[http://ivorylang.org](Ivory).
