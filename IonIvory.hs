e119f59b52c0a04e8504e4f0a8a56fe8ea881d31
Chris Hodapp <chris.hodapp@urbanalta.com>
2015-08-03 10:47:30 -0400

    Fixed incorrected generated code in last commit
    The code was doing a pointer dereference without a pointer being there; I
    knew about this and had put a note in the comments about it.

--- IonIvory.hs
+++ IonIvory.hs
@@ -38,32 +38,31 @@ ionProc scheds = (map incl $ entryProc : schedFns) ++
                  (map (defMemArea . counter) scheds)
   where schedFns :: [Def ('[] ':-> ())]
         schedFns = map mkSchedFn scheds
-        counter sch = area name $ Just $ ival (start :: Uint16)
+        counter sch = area name $ Just $ ival (start :: Uint32)
           where start = fromIntegral $ schedPhase sch
                 name = "counter_" ++ schedName sch
         mkSchedFn sch = proc ("ion_" ++ schedName sch) $ body $ do
           noReturn $ noBreak $ noAlloc $ getIvory sch
-        entryEff (sch, schFn) =
-          let start = fromIntegral $ (schedPeriod sch - 1)
-                      -- FIXME: Assign correct type to counter
-              ty = Ty.TyWord Ty.Word16
-              -- Counter variable:
-              var = AST.ExpSym $ memSym $ counter sch
-              -- Predicate, true if counter equals zero:
-              counterZero = AST.ExpOp (AST.ExpEq ty)
-                            [var, AST.ExpLit $ AST.LitInteger 0]
-              -- True case:
-              callSched = AST.Call Ty.TyVoid Nothing
-                          (AST.NameSym $ procName schFn) []
-              resetCount = AST.Store ty var $ AST.ExpLit $ AST.LitInteger start
-              -- False case:
-              decrCount = AST.Store ty var $
-                          (AST.ExpOp AST.ExpSub
-                           [var, AST.ExpLit $ AST.LitInteger 1])
-              -- FIXME: Both true and false case have a problem, which is that
-              -- AST.Store is storing to a reference, and 'var' is not a
-              -- pointer.
-          in emit $ AST.IfTE counterZero [callSched, resetCount] [decrCount]
+        entryEff (sch, schFn) = emit $
+                                AST.IfTE counterZero [callSched, reset] [decr]
+          where -- FIXME: Assign correct type to counter
+                ty = Ty.TyWord Ty.Word32
+                -- Counter variable:
+                var = AST.ExpSym $ memSym $ counter sch
+                -- Pointer to it (because AST.Store assumes a reference):
+                var' = AST.ExpAddrOfGlobal $ memSym $ counter sch
+                -- Predicate, true if counter equals zero:
+                counterZero = AST.ExpOp (AST.ExpEq ty)
+                              [var, AST.ExpLit $ AST.LitInteger 0]
+                -- True case:
+                callSched = AST.Call Ty.TyVoid Nothing
+                            (AST.NameSym $ procName schFn) []
+                reset = AST.Store ty var' $ AST.ExpLit $
+                        AST.LitInteger $ fromIntegral (schedPeriod sch - 1)
+                -- False case:
+                decr = AST.Store ty var' $
+                       (AST.ExpOp AST.ExpSub
+                        [var, AST.ExpLit $ AST.LitInteger 1])
         entryProc = proc "start_ion_" $ body $ do
           mapM_ entryEff $ zip scheds schedFns
           -- TODO: Disambiguate the name of this procedure
