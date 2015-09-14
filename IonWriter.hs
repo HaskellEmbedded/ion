123f3a33ba7ccd60814c85cd8d48374594488a26
Chris Hodapp <chris.hodapp@urbanalta.com>
2015-09-14 15:55:28 -0400

    Implemented flattening, again.

--- IonWriter.hs
+++ IonWriter.hs
@@ -3,13 +3,17 @@
 
 module IonWriter where
 
+import           Control.Exception
 import           Control.Monad.Writer
 import qualified Data.Tree as Tree
+import           Data.Typeable
 
 import qualified Ivory.Language as IL
 import qualified Ivory.Language.Monad as ILM
 import           Ivory.Language.Proc ( Def(..), Proc(..), IvoryCall_,
                                        IvoryProcDef )
+
+import           IonUtil
 
 -- | The type of Ivory action that an 'IonNode' can support. Note that this
 -- purposely forbids breaking, returning, and allocating.
@@ -43,7 +47,6 @@ data IonAction = IvoryEff (IvoryAction ()) -- ^ The Ivory effects that this
                  -- this node which must return 'true' for the node *and* for
                  -- any sub-nodes to execute their actions
                | Disable -- ^ Disable this node and all children
-               | NoAction -- ^ Do nothing
                deriving (Show)
 
 type IonTree = Tree.Tree IonAction
@@ -72,12 +75,72 @@ cond = addAction . AddCondition
 disable :: Ion a -> Ion a
 disable = addAction Disable
 
--- I don't know why you'd need this.
-nothing :: Ion a -> Ion a
-nothing = addAction NoAction
-
 ivoryEff :: (IvoryAction ()) -> Ion a -> Ion a
 ivoryEff = addAction . IvoryEff
+
+-- | A scheduled action.  Phase and period here are absolute, and there are no
+-- child nodes.
+data Schedule =
+  Schedule { schedId :: Integer -- ^ A unique ID for this action
+           , schedName :: String -- ^ Name (without any disambiguation applied)
+           , schedPath :: [String] -- ^ A list of names giving the trail that
+             -- produced this schedule
+           , schedPhase :: Integer -- ^ The (absolute & exact) phase of this
+             -- action
+           , schedPeriod :: Integer -- ^ The period of this action
+           , schedAction :: [IvoryAction ()] -- ^ The Ivory effects for this
+                            -- action
+           , schedCond :: [IvoryAction IL.IBool] -- ^ Ivory effects which all
+                          -- must return 'true' for anything in 'schedAction'
+                          -- to execute
+           }
+  deriving (Show)
+
+defaultSchedule = Schedule { schedId = 0
+                           , schedName = "root"
+                           , schedPath = []
+                           , schedPhase = 0
+                           , schedPeriod = 1
+                           , schedAction = []
+                           , schedCond = []
+                           }
+
+-- | Transform a 'Schedule' according to an 'IonAction'.
+modSchedule :: IonAction -> Schedule -> Schedule
+modSchedule (IvoryEff _) s = s
+modSchedule (SetPhase t _ ph) s =
+  if (ph' >= schedPeriod s)
+  then throw $ PhaseExceedsPeriod (schedPath s) ph' (schedPeriod s)
+  else s { schedPhase = ph' }
+  where ph' = case t of Absolute -> ph
+                        Relative -> schedPhase s + ph
+modSchedule (SetPeriod p) s = s { schedPeriod = fromIntegral p }
+modSchedule (SetName name) s =
+  case checkCName name of Just i -> throw $ InvalidCName (schedPath s) name i
+                          Nothing -> s { schedName = name
+                                       , schedPath = schedPath s ++ [name]
+                                       }
+modSchedule (AddCondition iv) s = s { schedCond = iv : schedCond s }
+-- FIXME: Handle exact and minimum phase.
+
+flattenTree :: Schedule -> IonTree -> [Schedule]
+flattenTree ctxt (Tree.Node action forest) = this : rest
+  where this = modSchedule action ctxt
+        rest = join $ map (flattenTree this) forest
+
+flatten :: Ion () -> [Schedule]
+flatten i = join $ map (flattenTree defaultSchedule) $ execWriter i
+
+prune :: [Schedule] -> [Schedule]
+prune = filter undefined
+
+data IonException = InvalidCName [String] String Int -- ^ Path, C name, and
+                    -- index at which it is invalid
+                  | PhaseExceedsPeriod [String] Integer Integer -- ^ Path,
+                    -- phase, period
+    deriving (Show, Typeable)
+
+instance Exception IonException
 
 test :: Ion ()
 test = do
